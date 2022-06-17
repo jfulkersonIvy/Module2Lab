b358958cf102ed875145b5cc6c71d7bbea56dafa
jfulkersonIvy <98554445+jfulkersonIvy@users.noreply.github.com>
2022-06-16 22:18:23 -0400

    Add comments

--- lab.py
+++ lab.py
@@ -3,14 +3,21 @@
 # Module 2 Lab - Case Study: if...else and while
 # This program will take a first and last name as input, along with a GPA. Then it will determine if that student made the Dean's List or the Honor Roll
 
+# This will allow us to exit the program if "ZZZ" is entered
 loop = True
 while loop is True:
     lastName = input("Please enter the student's last name (Enter ZZZ to quit): ")
+
+    # If "ZZZ" is the last name, exit the program
     if lastName == "ZZZ":
         loop = False
+
+    # If anything else is entered, continue
     else:
         firstName = input("Please enter the student's first name: ")
         gpa = float(input("Enter the student's GPA: "))
+
+        # Depending on the GPA, we display corresponding messages
         if gpa >= 3.5:
             print(f"{firstName} {lastName} has made the Dean's List!")
         elif 3.25 <= gpa < 3.5:
